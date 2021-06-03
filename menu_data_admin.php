d9108afadd4b18cf45b238ac4be212b4f9b546bf
derrydwi <derrydwiaditya@gmail.com>
2021-06-03 15:24:58 +0700

    update layout menu data

--- menu_data_admin.php
+++ menu_data_admin.php
@@ -9,48 +9,48 @@ $data_admin = query("SELECT * FROM admin WHERE username = '$username'")[0];
 <html lang="en">
 
 <head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-  <!-- Bootstrap CSS -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous" />
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous" />
 
-  <!-- Google Font -->
-  <link rel="preconnect" href="https://fonts.gstatic.com">
-  <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet">
+    <!-- Google Font -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet">
 
-  <!-- Page Icon -->
-  <link rel="icon" href="img/Logo_PSM.png">
+    <!-- Page Icon -->
+    <link rel="icon" href="img/Logo_PSM.png">
 
-  <!-- Font Awesome -->
-  <script src="https://kit.fontawesome.com/2b372b5a26.js" crossorigin="anonymous"></script>
+    <!-- Font Awesome -->
+    <script src="https://kit.fontawesome.com/2b372b5a26.js" crossorigin="anonymous"></script>
 
-  <!-- We CSS -->
-  <link rel="stylesheet" href="style.css" />
-  <link rel="stylesheet" href="styles.css" />
+    <!-- We CSS -->
+    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="styles.css" />
 
-  <!-- Jquery -->
-  <link href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css" rel="stylesheet">
-  <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
-  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js" integrity="sha256-T0Vest3yCU7pafRw9r+settMBX6JkKN06dqBnpQ8d30=" crossorigin="anonymous"></script>
+    <!-- Jquery -->
+    <link href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js" integrity="sha256-T0Vest3yCU7pafRw9r+settMBX6JkKN06dqBnpQ8d30=" crossorigin="anonymous"></script>
 
-  <script>
-    // Animasi fadeout dan auto close untuk alert
-    window.setTimeout(function() {
-      $('.alert')
-        .fadeTo(500, 0)
-        .slideUp(500, function() {
-          $(this).remove();
-        });
-    }, 2500);
-  </script>
+    <script>
+        // Animasi fadeout dan auto close untuk alert
+        window.setTimeout(function() {
+            $('.alert')
+                .fadeTo(500, 0)
+                .slideUp(500, function() {
+                    $(this).remove();
+                });
+        }, 2500);
+    </script>
 
-  <title>Prisai Sakti Mataram</title>
+    <title>Prisai Sakti Mataram</title>
 </head>
 
 <body class="sb-nav-fixed">
-<nav class="sb-topnav navbar navbar-expand navbar-dark bg-danger">
+    <nav class="sb-topnav navbar navbar-expand navbar-dark bg-danger">
         <!-- Navbar Brand-->
         <!-- <a class="navbar-brand ps-3" href="index.html">Perisai Sakti Mataram</a> -->
         <a class="navbar-brand ps-2" href="#">
@@ -77,8 +77,8 @@ $data_admin = query("SELECT * FROM admin WHERE username = '$username'")[0];
         </ul>
     </nav>
 
-  <!-- Navbar  Kanan-->
-  <div id="layoutSidenav">
+    <!-- Navbar  Kanan-->
+    <div id="layoutSidenav">
         <div id="layoutSidenav_nav">
             <nav class="sb-sidenav accordion sb-sidenav-dark bg-dark" id="sidenavAccordion">
                 <div class="sb-sidenav-menu">
@@ -90,7 +90,7 @@ $data_admin = query("SELECT * FROM admin WHERE username = '$username'")[0];
                         </a>
                         <a class="nav-link" href="index.php">
                             <div class="sb-nav-link-icon"><i class="fas fa-home"></i></div>
-                            Website 
+                            Website
                         </a>
                         <div class="sb-sidenav-menu-heading">Menu</div>
 
@@ -154,32 +154,32 @@ $data_admin = query("SELECT * FROM admin WHERE username = '$username'")[0];
             </nav>
         </div>
         <!-- Navbar Kiri -->
-  <!-- Akhir Navbar  -->
+        <!-- Akhir Navbar  -->
 
-  <div id="layoutSidenav_content">
-    <main>
-      <div class="container-fluid px-4">
-  <h1 class="text-center my-3">Data Admin</h1>
-  <div class="container my-5">
-    <table>
-      <tr>
-        <td>Username</td>
-        <td>:</td>
-        <td><b><?= $data_admin['username'] ?></b></td>
-      </tr>
-      <tr>
-        <td>No. Telepon</td>
-        <td>:</td>
-        <td><b><?= $data_admin['no_telp'] ?></b></td>
-      </tr>
-    </table>
-  </div>
-</div>
-</main>
-</div>
+        <div id="layoutSidenav_content">
+            <main>
+                <div class="container-fluid px-4">
+                    <h1 class="text-center my-3">Data Admin</h1>
+                    <div class="container my-5">
+                        <table>
+                            <tr>
+                                <td>Username</td>
+                                <td>:</td>
+                                <td><b><?= $data_admin['username'] ?></b></td>
+                            </tr>
+                            <tr>
+                                <td>No. Telepon</td>
+                                <td>:</td>
+                                <td><b><?= $data_admin['no_telp'] ?></b></td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </main>
+        </div>
 
-  <!-- Option 1: Bootstrap Bundle with Popper -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+        <!-- Option 1: Bootstrap Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 </body>
 
 </html>
